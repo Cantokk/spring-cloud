35650d9052b8d7280e8ce5ff610b0dee26ea432d
Canto <jk5643@nyu.edu>
2020-08-22 14:31:25 -0700

    Enable Sleuth to generate global uuid for request tracing and adding more log

--- currency-exchange-service/src/main/java/com/springcloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/springcloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.springcloud.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private Environment environment;
@@ -28,6 +32,9 @@ public class CurrencyExchangeController {
         exchangeValue.setPort(
                 Integer.parseInt(environment.getProperty("local.server.port"))
         );
+
+        logger.info("{}", exchangeValue);
+
         return exchangeValue;
     }
 }
